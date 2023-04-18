30ef7da70d8fce99ad443d127fe6c7928d95ad4c
sabin bajracharya <sabin1129@hotmail.com>
2023-04-18 11:52:01 +0200

    React application created

---
+++ ems-crud/src/test/java/api/emscrud/EmsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package api.emscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
