a9dec3d831445c600f6cb91ec581813441a676ae
sabin bajracharya <sabin1129@hotmail.com>
2023-04-18 11:52:01 +0200

    React application created

---
+++ ems-crud/src/main/java/api/emscrud/EmsCrudApplication.java
@@ -0,0 +1,13 @@
+package api.emscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsCrudApplication.class, args);
+	}
+
+}
